d6660b180c948f616c137876156939ec22789e96
ThorW82 <torish82@gmail.com>
2022-12-12 20:07:22 +0200

    settings: +static_whitenoise

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 # AUTH_USER_MODEL = 'userprofile.User'
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
